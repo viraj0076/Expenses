4d8478d2b4e48a2df3c37365d6ed2364560d5f2b
Viraj Koradia <20it057@charusat.edu.in>
2023-03-03 13:41:46 +0530

    Final Expenses

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(enteredExpenseData);
+    console.log("This is the console log of the NewsExpense");
+    console.log(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
